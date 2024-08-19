5af59f8a97d66a2118ffd3c661398ef4cb90e930
Igor Galvão <igorgalvao092@gmail.com>
2024-08-19 16:58:12 +0000

    Atualização

--- app/src/main/java/application/controller/AlunoController.java
+++ app/src/main/java/application/controller/AlunoController.java
@@ -2,6 +2,7 @@ package application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +26,10 @@ public class AlunoController {
     public Aluno insert(@RequestBody Aluno aluno) {
         return alunoRepo.save(aluno);
 
+    }
+
+    @GetMapping("/{id}")
+    public Aluno details(@PathVariable long id) {
+        return alunoRepo.findById(id).get();
     }
 }
